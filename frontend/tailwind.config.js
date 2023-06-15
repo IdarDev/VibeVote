a536b0ccd5cee87b60e2c23c2da79fd1cdbde7f4
Idar <idarnigatu@gmail.com>
2023-06-15 15:04:06 +0200

    coming soon

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.html", "./src/**/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+      gridColumn: {
+        "span-8": "span 8 / span 8",
+      },
+    },
   },
   variants: {
     extend: {},
